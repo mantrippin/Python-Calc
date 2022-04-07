ecedcfc13b0d51fcc67e342c634fe04960262d7f
manstrippin <floattimfloat@gmail.com>
2022-04-07 12:27:34 +0000

    a lot

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
-print ("Welcome to my Python Calculator")
-print ("Type in your numbers")
-if 
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+print("Select Symbol.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply ")
+print("4.Divide ")
+
+while True:
+    
+    choice = input("Enter symbol choice(1/2/3/4): ")
+
+    
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second very cool number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        
+        
+        next_calculation = input("Do another operation?? (yes/no): ")
+        if next_calculation == "no":
+          print ("Made by manstrippin")
+          break
+    
+    else:
+     
+        print("Invalid Input")
