baa70b87d49fc5950de8d81f978bf193fd6484e2
manstrippin <floattimfloat@gmail.com>
2022-04-07 17:34:15 +0000

    Easier to understand

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def add(x, y):
     return x + y
-
+# This is for adding
 
 def subtract(x, y):
     return x - y
-
+#Subtracting
 
 def multiply(x, y):
     return x * y
-
+#Multiplying
 
 def divide(x, y):
     return x / y
-
+#And dividing
 
 print("Select Symbol.")
 print("1.Add")
@@ -28,19 +28,19 @@ while True:
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second very cool number: "))
-
+#Entering the number is here
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
-
+# Adding
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-
+# Subtracting
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-
+# Multiplying
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-        
+        # Dividing
         
         
         next_calculation = input("Do another operation?? (yes/no): ")
@@ -49,5 +49,6 @@ while True:
           break
     
     else:
-     
+     # By the way, I didn't fully make this on my own I had to search up a few things for help
         print("Invalid Input")
+      
